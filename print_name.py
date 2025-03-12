2d8289cbe4ed2b43f5496de03d1ddc2961bc2e03
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-03-12 22:45:40 +0600

    Create print_name.py

---
+++ print_name.py
@@ -0,0 +1,10 @@
+# You are given the firstname and lastname of a person 
+# on two different lines. Your task is to read them and
+#  print the following:
+
+def person_name(Fname,Lname):
+    print(f"The  Name Of the Person is {Fname} {Lname}")
+
+person_name("Amanullah","Sheikh")
+person_name("Apon","Sheikh")
+person_name("RiJve","Ahammed")
